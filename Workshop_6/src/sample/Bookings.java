5e69fb712da43cdb202cbe050ab266effcf1e946
SinKane <wade.grimm@shaw.ca>
2020-04-06 17:12:54 -0600

    First Commit

---
+++ Workshop_6/src/sample/Bookings.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Bookings {
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    void clickedCancel(ActionEvent event) throws IOException {
+        Parent stage = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene scene = new Scene((stage));
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+    }
+
+}
