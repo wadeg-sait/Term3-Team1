8846682de0e58ea9b1bbe5b60dfb894cb7ea4eb9
ibrahimHash <57676976+ibrahimHash@users.noreply.github.com>
2020-04-24 14:30:38 -0600

    Added my portion of the project

---
+++ Term3-Team1-master/Workshop_6/src/sample/Agents.java
@@ -0,0 +1,30 @@
+package sample;
+/*
+Base skeleton created by Wade Grimm
+ */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Agents {
+
+    @FXML
+    void clickedCancel(ActionEvent event) throws IOException {
+        Parent stage = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene scene = new Scene((stage));
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+    }
+
+
+
+
+
+}
---
+++ Term3-Team1-master/Workshop_6/src/sample/Controller.java
@@ -0,0 +1,69 @@
+package sample;
+/*
+Created by Wade Grimm
+ */
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller {
+
+    @FXML
+    private Button btnAgents;
+
+    @FXML
+    private Button btnSuppliers;
+
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private Button btnBookings;
+
+    @FXML
+    void clickedAgents(ActionEvent event) throws IOException {
+        Parent stage = FXMLLoader.load(getClass().getResource("agents.fxml"));
+        Scene scene = new Scene((stage));
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+    }
+
+    @FXML
+    void clickedBookings(ActionEvent event) throws IOException {
+
+        Parent stage = FXMLLoader.load(getClass().getResource("bookings.fxml"));
+        Scene scene = new Scene((stage));
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+    }
+
+    @FXML
+    void clickedCustomers(ActionEvent event) throws IOException {
+        Parent stage = FXMLLoader.load(getClass().getResource("customers.fxml"));
+        Scene scene = new Scene((stage));
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+    }
+
+    @FXML
+    void clickedSuppliers(ActionEvent event) throws IOException {
+        Parent stage = FXMLLoader.load(getClass().getResource("suppliers.fxml"));
+        Scene scene = new Scene((stage));
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+
+    }
+
+}
