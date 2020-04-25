40bbb6d4d0a851c94fd906dd59fb22b73fac2e55
SinKane <wade.grimm@shaw.ca>
2020-04-25 08:39:33 -0600

    Fixed the cancel button on Zohas form

--- Workshop_6/src/sample/BookingInfo/BookingDetail.java
+++ Workshop_6/src/sample/BookingInfo/BookingDetail.java
@@ -3,13 +3,11 @@ package sample.BookingInfo;
 Created by Wade Grimm
  */
 
-import javafx.beans.property.SimpleIntegerProperty;
+
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+
 
 import java.sql.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 
 public class BookingDetail {
 
