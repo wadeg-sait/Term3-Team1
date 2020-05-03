792296d7034666627fa5dc1364ea64d772d7ab6f
Zoha Ahmed <835909@ACDM.DS.SAIT.CA>
2020-05-02 20:28:28 -0600

    rest api are updated

--- Workshop7/src/main/BookingsRestService.java
+++ Workshop7/src/main/BookingsRestService.java
@@ -5,6 +5,15 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.FormParam;
@@ -12,9 +21,39 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
-@Path("/booking")
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import model.Agent;
+import model.Booking;
+
+
+@Path("/bookings")
 public class BookingsRestService {
+	
+	BookingDBConnection bookdb= new BookingDBConnection();
+	
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Bookings> getBookings()
+	
+	{	
+		System.out.println("Test");
+		return bookdb.getBookings();
+
+	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Bookings getBooking(@PathParam("id") int id)
+	{
+		return bookdb.getBooking(id);
+		
+	}
 
 	@GET
 	@Path("/get")
@@ -25,28 +64,83 @@ public class BookingsRestService {
         return "test123";	
 	}
 	
-	
-	
+
 
 	@POST
-	@Path("/<add method name here>")
+	@Path("/postBooking")
     @Produces(MediaType.TEXT_PLAIN)
-	public String postSomething(@FormParam("request") String request ,  @DefaultValue("1") @FormParam("version") int version) {
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String postSomething(String jsonString) {
+		
+	
+		EntityManagerFactory factory =
+		Persistence.createEntityManagerFactory("Workshop7");
+		EntityManager em =
+		 factory.createEntityManager();
+		
 
-        return "post";	
+		Gson gson = new Gson();
+		Booking booking = gson.fromJson(jsonString, Booking.class);
+		
+		System.out.println(booking);
+	
+		em.getTransaction().begin(); 
+		Booking newBooking = em.merge(booking);
+		em.getTransaction().commit(); em.close(); factory.close();
+		 
+        return "Booking has been posted";	
 	}
 
 	@PUT
-	@Path("/<add method name here>")
-    @Produces(MediaType.TEXT_PLAIN)
-	public String putSomething(@FormParam("request") String request ,  @DefaultValue("1") @FormParam("version") int version) {
+	@Path("/updateBooking")
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String updateBooking(String jsonString)
 		
-        return "put";	
+	
+	{
+		
+		  EntityManagerFactory factory =
+		  Persistence.createEntityManagerFactory("Workshop7"); EntityManager em =
+		  factory.createEntityManager();
+		 
+
+		Gson gson = new Gson();
+		//Type type = new TypeToken<Agent>() {}.getType();
+		Booking booking = gson.fromJson(jsonString, Booking.class);
+		 
+		System.out.println(booking.getBookingNo());
+		
+		  em.getTransaction().begin(); em.persist(booking);
+		  em.getTransaction().commit(); em.close(); factory.close();
+		 
+
+			 
+		return "Booking has been Updated";
 	}
 
-	@DELETE
-	@Path("/<add method name here>")
-	public void deleteSomething(@FormParam("request") String request ,  @DefaultValue("1") @FormParam("version") int version) {
+	@DELETE 
+	@Path("deleteBooking/{id}")
+	public String deleteBooking(@PathParam("id") int id)
+	
+	{
+		Bookings booking= bookdb.getBooking(id);
+		if (booking.getBookingId()!=0)
+		{
+			bookdb.deleteBooking(id);
+			if (bookdb.getBooking(booking.getBookingId()).getBookingId()!=0)
+			{
+				return "There was an error while deleting this booking";
+			}
+			
+			else {
+				return "Booking deleted";
+			}
+			
+		}
+		
+		return "Booking not present in table";
+			
 	
 	}
 }
